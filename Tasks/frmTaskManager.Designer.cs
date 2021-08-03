1cd6a6bbd3e875098054465e0a3b811cda0237e9
byron <53088136+byronbytes@users.noreply.github.com>
2021-08-03 10:30:04 -0400

    Task Manager might work in its earliest form

---
+++ Tasks/frmTaskManager.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Tasks
+{
+    partial class frmTaskManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader6 = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.ShowItemToolTips = true;
+            this.listView1.Size = new System.Drawing.Size(656, 663);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Process Name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "PID";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Status";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "User Running";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Memory";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Process Description";
+            // 
+            // frmTaskManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(828, 687);
+            this.Controls.Add(this.listView1);
+            this.Name = "frmTaskManager";
+            this.Text = "frmTaskManager";
+            this.Load += new System.EventHandler(this.frmTaskManager_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
---
+++ Tasks/frmTaskManager.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Dynamic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tasks
+{
+    public partial class frmTaskManager : Form
+    {
+        public frmTaskManager()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Returns an Expando object with the description and username of a process from the process ID.
+        /// </summary>
+        /// <param name="processId"></param>
+        /// <returns></returns>
+        public ExpandoObject GetProcessExtraInformation(int processId)
+        {
+            // Query the Win32_Process
+            string query = "Select * From Win32_Process Where ProcessID = " + processId;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = searcher.Get();
+
+            // Create a dynamic object to store some properties on it
+            dynamic response = new ExpandoObject();
+            response.Description = "";
+            response.Username = "System";
+
+            foreach (ManagementObject obj in processList)
+            {
+                // Retrieve username 
+                string[] argList = new string[] { string.Empty, string.Empty };
+                int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                if (returnVal == 0)
+                {
+                    // return Username
+                    response.Username = argList[0];
+
+                    // You can return the domain too like (PCDesktop-123123\Username using instead
+                    //response.Username = argList[1] + "\\" + argList[0];
+                }
+
+                // Retrieve process description if exists
+                if (obj["ExecutablePath"] != null)
+                {
+                    try
+                    {
+                        FileVersionInfo info = FileVersionInfo.GetVersionInfo(obj["ExecutablePath"].ToString());
+                        response.Description = info.FileDescription;
+                    }
+                    catch { }
+                }
+            }
+
+            return response;
+        }
+        /// <summary>
+        /// Method that converts bytes to its human readable value
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public string BytesToReadableValue(long number)
+        {
+            List<string> suffixes = new List<string> { " B", " KB", " MB", " GB", " TB", " PB" };
+
+            for (int i = 0; i < suffixes.Count; i++)
+            {
+                long temp = number / (long)Math.Pow(1024, i + 1);
+
+                if (temp == 0)
+                {
+                    return (number / (long)Math.Pow(1024, i)) + suffixes[i];
+                }
+            }
+
+            return number.ToString();
+        }
+
+        public void renderProcessesOnListView()
+        {
+            // Create an array to store the processes
+            Process[] processList = Process.GetProcesses();
+
+            // Create an Imagelist that will store the icons of every process
+            ImageList Imagelist = new ImageList();
+
+            // Loop through the array of processes to show information of every process in your console
+            foreach (Process process in processList)
+            {
+                // Define the status from a boolean to a simple string
+                string status = (process.Responding == true ? "Responding" : "Not Responding");
+
+                // Retrieve the object of extra information of the process (to retrieve Username and Description)
+                dynamic extraProcessInfo = GetProcessExtraInformation(process.Id);
+
+                // Create an array of string that will store the information to display in our 
+                string[] row = {
+            // 1 Process name
+            process.ProcessName,
+            // 2 Process ID
+            process.Id.ToString(),
+            // 3 Process status
+            status,
+            // 4 Username that started the process
+            extraProcessInfo.Username,
+            // 5 Memory usage
+            BytesToReadableValue(process.PrivateMemorySize64),
+            // 6 Description of the process
+            extraProcessInfo.Description
+        };
+
+                //
+                // As not every process has an icon then, prevent the app from crash
+                try
+                {
+                    Imagelist.Images.Add(
+                        // Add an unique Key as identifier for the icon (same as the ID of the process)
+                        process.Id.ToString(),
+                        // Add Icon to the List 
+                        System.Drawing.Icon.ExtractAssociatedIcon(process.MainModule.FileName).ToBitmap()
+                    );
+                }
+                catch
+                {
+                    var bitmap = new Bitmap(256, 256);
+                    using Graphics gdi = Graphics.FromImage(bitmap);
+                    gdi.DrawString("Cringe", Font.Name., 20);
+                    gdi.Save();
+                    //bitmap is now your placeholder icon
+
+                    MessageBox.Show("A");
+                }
+
+                // Create a new Item to add into the list view that expects the row of information as first argument
+                ListViewItem item = new ListViewItem(row)
+                {
+                    // Set the ImageIndex of the item as the same defined in the previous try-catch
+                    ImageIndex = Imagelist.Images.IndexOfKey(process.Id.ToString())
+                };
+
+                // Add the Item
+                listView1.Items.Add(item);
+            }
+
+            // Set the imagelist of your list view the previous created list :)
+            listView1.LargeImageList = Imagelist;
+            listView1.SmallImageList = Imagelist;
+        }
+
+        private void frmTaskManager_Load(object sender, System.EventArgs e)
+        {
+            renderProcessesOnListView();
+        }
+ 
+        
+    }
+}
