874f54094f955fffee65612b773b5ecdf1e9d784
Byron <53088136+byronbytes@users.noreply.github.com>
2021-08-05 19:07:03 -0400

    Update TakeownTempFiles.bat

--- Tasks/Utility Scripts/TakeownTempFiles.bat
+++ Tasks/Utility Scripts/TakeownTempFiles.bat
@@ -1,10 +1,12 @@
 @echo off 
 color 02
+echo -----------------------------------------Warning:---------------------------------------------
 echo This script will take ownership of the directories C:\Windows\Temp and C:\Windows\Prefetch.
 echo You may need to run this prompt as administrator.
-pause 1
+echo If you do not want to do this, you can close this prompt and nothing will happen.
+echo -----------------------------------------Warning:---------------------------------------------
+pause
 takeown /F "C:\Windows\Temp" /A /R /D Y
 takeown /F "C:\Windows\Prefetch" /A /R /D Y
-
 echo The task finished successfully. Click anywhere to close.
-pause 1
+pause 
