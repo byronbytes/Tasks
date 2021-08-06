dc1cdb27c508cea8c639bf35949c45fcf7d05fde
averageftw <67998246+averageftw@users.noreply.github.com>
2021-08-06 22:12:50 +0700

    frmSettings.cs | CODE STYLE CHANGE

--- Tasks/frmSettings.cs
+++ Tasks/frmSettings.cs
@@ -8,34 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Tasks
-{
-    public partial class frmSettings : Form
-    {
-        public frmSettings()
-        {
-            InitializeComponent();
-        }
+namespace Tasks {
+    public partial class frmSettings : Form {
+        public frmSettings() { InitializeComponent(); }
 
+        private void button2_Click(object sender, EventArgs e) { taskDialog1.Show(); }
 
-
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            taskDialog1.Show();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
+        private void button4_Click(object sender, EventArgs e) {
             frmMain Main = new frmMain(); // Initialize form.
             Main.panel1.BackColor = Color.FromArgb(240, 240, 245);
             Main.label1.BackColor = Color.Black;
-
         }
 
-        private void frmSettings_Load(object sender, EventArgs e)
-        {
-
-        }
+        private void frmSettings_Load(object sender, EventArgs e) {}
     }
 }
