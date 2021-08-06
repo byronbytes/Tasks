dbe041708d1feb1419fade4ecea4dab3f9b71d99
averageftw <67998246+averageftw@users.noreply.github.com>
2021-08-06 22:08:14 +0700

    frmMain.cs | UPDATE CODE STYLE | Check description
    It might possibly break, as i changed:
    ```cs
    var varName = new className();
    varName.classMethod();
    ```
    to
    ```cs
    new className().classMethod();
    ```

--- Tasks/frmMain.cs
+++ Tasks/frmMain.cs
@@ -9,52 +9,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Tasks
-{
-    public partial class frmMain : Form
-    {
-        public frmMain()
-        {
-            InitializeComponent();
-        }
+namespace Tasks {
+    public partial class frmMain : Form {
+        public frmMain() { InitializeComponent(); }
 
-        private void frmMain_Load(object sender, EventArgs e)
-        {
-      // note for tomorrow, add a file system for tasks temp files and more, reg keys etc
+        // TODO: add registry keys and filesystem and other stuff
+        private void frmMain_Load(object sender, EventArgs e) {
         }
-        private void button1_Click(object sender, EventArgs e)
-        {
+        
+        private void button1_Click(object sender, EventArgs e) {
             frmCleanup Cleanup = new frmCleanup(); // Initialize cleanup form.
             Cleanup.Show(); // Show Form
         }
 
-        private void button4_Click(object sender, EventArgs e)
-        {
+        private void button4_Click(object sender, EventArgs e) {
             frmTaskManager TaskManager = new frmTaskManager(); // Initialize cleanup form.
             TaskManager.Show(); // Show Form
         }
 
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
+        private void pictureBox2_Click(object sender, EventArgs e) {
             Process.Start(new ProcessStartInfo { FileName = "https://twitter.com/Lite_Tools", UseShellExecute = true });
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-            Process.Start(new ProcessStartInfo{FileName = "https://discord.gg/nCBD9ZJjng", UseShellExecute = true});
-
+        private void pictureBox1_Click(object sender, EventArgs e) {
+            Process.Start(new ProcessStartInfo { FileName = "https://discord.gg/nCBD9ZJjng", UseShellExecute = true});
         }
 
-        private void button6_Click(object sender, EventArgs e)
-        {
-            frmUtilityScripts UtilityScripts = new frmUtilityScripts(); // Initialize cleanup form.
-            UtilityScripts.Show(); // Show Form
+        private void button6_Click(object sender, EventArgs e) {
+            new frmUtilityScripts().show(); 
         }
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-            frmSettings Settings = new frmSettings(); // Initialize cleanup form.
-            Settings.Show(); // Show Form
+        private void button5_Click(object sender, EventArgs e) {
+            new frmSettings().show(); 
         }
     }
 }
