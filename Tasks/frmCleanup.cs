77548636dc11ccd3fd10c0e59b9d3482f77827b3
averageftw <67998246+averageftw@users.noreply.github.com>
2021-08-06 22:01:01 +0700

    Update frmCleanup.cs

--- Tasks/frmCleanup.cs
+++ Tasks/frmCleanup.cs
@@ -12,19 +12,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Tasks
-{
-    public partial class frmCleanup : Form
-    {
-        public frmCleanup()
-        {
-            InitializeComponent();
-        }
+namespace Tasks {
+    public partial class frmCleanup : Form {
+        public frmCleanup() { InitializeComponent(); }
 
         [DllImport("Shell32.dll")]
         static extern int SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, RecycleFlag dwFlags);
-        enum RecycleFlag : int
-        {
+        enum RecycleFlag : int {
             SHERB_NOCONFIRMATION = 0x00000001, // No confirmation, when emptying
             SHERB_NOPROGRESSUI = 0x00000001, // No progress tracking window during the emptying of the recycle bin
             SHERB_NOSOUND = 0x00000004 // No sound when the emptying of the recycle bin is complete
@@ -32,54 +26,44 @@ namespace Tasks
 
 
 
-        private bool DeleteAllFiles(DirectoryInfo directoryInfo)
-        {
-            try
-            {
-                foreach (var file in directoryInfo.GetFiles())
-                {
+        private bool DeleteAllFiles(DirectoryInfo directoryInfo) {
+            try {
+                foreach (var file in directoryInfo.GetFiles()) {
                     file.Delete();
                     CleanupLogsLBox.Items.Add("Deleted File " + file.FullName);
                 }
 
-                foreach (var dir in directoryInfo.GetDirectories())
-                {
+                foreach (var dir in directoryInfo.GetDirectories()) {
                     dir.Delete(true);
                     CleanupLogsLBox.Items.Add("Deleted Folder " + dir.FullName);
                 }
 
                 return true;
             }
-            catch (Exception ex) when (ex is IOException || ex is DirectoryNotFoundException || ex is UnauthorizedAccessException || ex is SecurityException)
-            {
+            catch (Exception ex) when (ex is IOException || ex is DirectoryNotFoundException || ex is UnauthorizedAccessException || ex is SecurityException) {
                 CleanupLogsLBox.Items.Add("Exception Error: " + ex.Message);
             }
 
             return false;
         }
 
-        private void btnCleanup_Click(object sender, EventArgs e)
-        {
-            if (!checkBox1.Checked && !checkBox2.Checked && !checkBox3.Checked && !checkBox4.Checked)
-            {
+        private void btnCleanup_Click(object sender, EventArgs e) {
+            if (!checkBox1.Checked && !checkBox2.Checked && !checkBox3.Checked && !checkBox4.Checked) {
                 CleanupLogsLBox.Items.Add("Please select something to clean!");
                 return;
             }
             
-            if (checkBox1.Checked)
-            {
+            if (checkBox1.Checked) {
                 var directory = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads");
                 if (DeleteAllFiles(directory)) CleanupLogsLBox.Items.Add("Downloads Folder Cleaned.");
             }
 
-            if (checkBox2.Checked)
-            {
+            if (checkBox2.Checked) {
                 SHEmptyRecycleBin(IntPtr.Zero, null, RecycleFlag.SHERB_NOSOUND | RecycleFlag.SHERB_NOCONFIRMATION);
                 CleanupLogsLBox.Items.Add("Recycle Bin Cleaned.");
             }
 
-            if (checkBox3.Checked)
-            {
+            if (checkBox3.Checked) {
                 var windowstemp = new DirectoryInfo("C:\\Windows\\Temp");
                 var usertemp = new DirectoryInfo(Path.GetTempPath());
                 
@@ -87,8 +71,7 @@ namespace Tasks
                 if (DeleteAllFiles(usertemp)) CleanupLogsLBox.Items.Add("User Temp Folder Cleaned.");
             }
 
-            if (checkBox4.Checked)
-            {
+            if (checkBox4.Checked) {
                 var directory = new DirectoryInfo("C:\\Windows\\Prefetch");
                 if (DeleteAllFiles(directory)) CleanupLogsLBox.Items.Add("Prefetch Cleaned.");
             }
@@ -96,14 +79,12 @@ namespace Tasks
             if (CleanupLogsLBox.Items.Count > 1) btnCopyLogs.Enabled = true;
         }
 
-        private void btnCopyLogs_Click(object sender, EventArgs e)
-        {
+        private void btnCopyLogs_Click(object sender, EventArgs e) {
             CleanupLogsLBox.Items.Add("Debug log copied to clipboard.");
             Clipboard.SetText(string.Join("\n", CleanupLogsLBox.Items.Cast<string>()));
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+        private void button1_Click(object sender, EventArgs e) {
             Process.Start(new ProcessStartInfo { FileName = "https://github.com/LiteTools/Tasks/wiki/Cleanup:-What-do-these-mean%3F", UseShellExecute = true });
         }
     }
