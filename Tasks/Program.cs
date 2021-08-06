bd66b198e7df9924870a268e75a514cc28dbcf78
averageftw <67998246+averageftw@users.noreply.github.com>
2021-08-06 21:29:34 +0700

    Program.cs | CODE STYLE CHANGE

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -4,16 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Tasks
-{
-    static class Program
-    {
+namespace Tasks {
+    static class Program {
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
-        {
+        static void Main() {
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
