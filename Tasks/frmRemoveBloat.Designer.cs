ecf73d8948ed7911f892c8ca74c9c3e07fa8b0be
Byron <53088136+byronbytes@users.noreply.github.com>
2021-08-13 21:36:05 -0400

    Desgining

--- Tasks/frmRemoveBloat.Designer.cs
+++ Tasks/frmRemoveBloat.Designer.cs
@@ -30,23 +30,38 @@ namespace Tasks
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRemoveBloat));
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(421, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This will run the Windows10Debloater script, credit goes to the original creator." +
+    "\r\n\r\nhttps://github.com/Sycnex/Windows10Debloater";
             // 
             // frmRemoveBloat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
-            this.ClientSize = new System.Drawing.Size(819, 607);
+            this.ClientSize = new System.Drawing.Size(558, 451);
+            this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.Color.White;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmRemoveBloat";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Remove Bloatware";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
