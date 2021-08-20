e4a6989f38e1d56088491dfb7280b3875c0e65d4
Byron <53088136+byronbytes@users.noreply.github.com>
2021-08-20 17:24:08 -0400

    Changes

--- Tasks/WIP/frmOverlay.cs
+++ Tasks/WIP/frmOverlay.cs
@@ -20,80 +20,9 @@ namespace Tasks.WIP
 
         private void frmOverlay_Load(object sender, EventArgs e)
         {
-            GetGPUUsage();
+     
         }
-        public static async Task<float> GetGPUUsage()
-        {
-            try
-            {
-                var category = new PerformanceCounterCategory("GPU Engine");
-                var counterNames = category.GetInstanceNames();
-                var gpuCounters = new List<PerformanceCounter>();
-                var result = 0f;
-
-                foreach (string counterName in counterNames)
-                {
-                    if (counterName.EndsWith("engtype_3D"))
-                    {
-                        foreach (PerformanceCounter counter in category.GetCounters(counterName))
-                        {
-                            if (counter.CounterName == "Utilization Percentage")
-                            {
-                                gpuCounters.Add(counter);
-                            }
-                        }
-                    }
-                }
-
-                gpuCounters.ForEach(x =>
-                {
-                    _ = x.NextValue();
-                });
-
-                await Task.Delay(1000);
-
-                gpuCounters.ForEach(x =>
-                {
-                    result += x.NextValue();
-                });
-
-                return result;
-            }
-            catch
-            {
-                return 0f;
-            }
-        }
-
-        public static async Task<float> GetCPUUsage()
-        {
-            try
-            {
-                PerformanceCounter cpuCounter =
-                    new PerformanceCounter("Processor", "% Processor Time", "_Total");
-                _ = cpuCounter.NextValue();
-                await Task.Delay(1000);
-                return cpuCounter.NextValue();
-            }
-            catch
-            {
-                return 0f;
-            }
-        }
-
-        public static float GetMemoryUsage()
-        {
-            try
-            {
-                PerformanceCounter memCounter =
-                    new PerformanceCounter("Memory", "Available MBytes");
-                return memCounter.NextValue();
-            }
-            catch
-            {
-                return 0f;
-            }
-        }
+      
 
 
         public const int WM_NCLBUTTONDOWN = 0xA1;
