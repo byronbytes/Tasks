d5541d8ffcbb9ee48d1462eda47172bd6173234a
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-05 13:23:22 -0400

    Create Cleanup.cs

---
+++ Tasks/Tasks v3.0.0/Classes/Cleanup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Cleanup_Modules
+{
+    public class Cleanup
+    {
+        // TODO: I have no idea
+
+        public Dictionary<string, string> Folders
+        {
+            get; set;
+        }
+
+
+      
+
+
+
+    }
+    }
+
