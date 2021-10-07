c71c2b091468055397c528a321b9a2744137a9f8
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-07 07:19:16 -0400

    attemped to fix most bugs created at school

--- Tasks/Tasksv3/Classes/Cleanup.cs
+++ Tasks/Tasksv3/Classes/Cleanup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,79 +9,82 @@ using System.Threading.Tasks;
 namespace Tasks.Cleanup_Modules
 {
     public class Cleanup
-    {    
-
-  // Strings for the subdir and cachedirs will be listed on top, and the clean will be able to select the directories, so if chromeSubDir = subdir in the main code,
-         // it should clean the chrome cache. This will be more efficient since you wouldn't need to repeat the code 10000 times. Although, after a cleanup it will need
-            // to clear the strings so it doesn't mess up and delete other files.
-            // This could be fixed by saying if(checkbox is checked then string = appsubdir, appcachedir)
-    
-     public string chromeSubDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\Google\\Chrome\\User Data\\";
-     public string[] chromeCacheDir = { "Default\\Cache", "Default\\Code Cache\\", "Default\\GPUCache", "ShaderCache", "Default\\Service Worker\\CacheStorage", "Default\\Service Worker\\ScriptCache", "GrShaderCache\\GPUCache", "\\Default\\File System\\" };
-        
-     public string[] windowsTemp = { "C:\\Windows\\Temp", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\Temp" } // might be incorrect.
-    
-        
-        
-        public void Clean(string subdir, string cacheDir) 
-        {      
-            List<DirectoryInfo> directoryInfos = new List<DirectoryInfo>();
-                    foreach (string subdir in cacheDir)
-                    {
-                        directoryInfos.Add(new DirectoryInfo(mainSubdirectory + subdir + "\\"));
-                    }
-
-                    bool isDeleted = true;
-                    // For each DirectoryInfo inside of the directoryInfos array
-                    foreach (DirectoryInfo d in directoryInfos)
-                    {
-                        try
-                        {
-                            Delete(d);
-                        }
-                        catch (Exception ex)
-                        {
-                          
-                        }
-
-                        // If DeleteAllFiles returns false, set the isDeleted value to false
-                        if (!DeleteAllFiles(d)) isDeleted = false;
-              
-                        }
-        }
-        
-    
-    public bool Delete(DirectoryInfo dirInfo) // idk yet will work on at home if i can
     {
-        foreach (var file in directoryInfo.GetFiles())
+
+        // Strings for the subdir and cachedirs will be listed on top, and the clean will be able to select the directories, so if chromeSubDir = subdir in the main code,
+        // it should clean the chrome cache. This will be more efficient since you wouldn't need to repeat the code 10000 times. Although, after a cleanup it will need
+        // to clear the strings so it doesn't mess up and delete other files.
+        // This could be fixed by saying if(checkbox is checked then string = appsubdir, appcachedir)
+
+        public string chromeSubDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\Google\\Chrome\\User Data\\";
+        public string[] chromeCacheDir = { "Default\\Cache", "Default\\Code Cache\\", "Default\\GPUCache", "ShaderCache", "Default\\Service Worker\\CacheStorage", "Default\\Service Worker\\ScriptCache", "GrShaderCache\\GPUCache", "\\Default\\File System\\" };
+
+        public string[] windowsTemp = { "C:\\Windows\\Temp", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\Temp" }; // might be incorrect.
+
+
+
+        public void Clean(string subdir, string cacheDir)
+        {
+            List<DirectoryInfo> directoryInfos = new List<DirectoryInfo>();
+
+            foreach (string subdir in cacheDir)
+            {
+                directoryInfos.Add(new DirectoryInfo(subdir + subdir + "\\"));
+            }
+
+            bool isDeleted = true;
+            // For each DirectoryInfo inside of the directoryInfos array
+            foreach (DirectoryInfo d in directoryInfos)
+            {
+                try
+                {
+                    Delete(d);
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+                // If DeleteAllFiles returns false, set the isDeleted value to false
+                if (!Delete(d)) isDeleted = false;
+
+            }
+        }
+
+
+        public static bool Delete(DirectoryInfo dirInfo) // idk yet will work on at home if i can
+        {
+            foreach (var file in directoryInfo.GetFiles())
             {
                 try
                 {
                     file.Delete();
-                    Debug.Log("Deleted " + file.FullName);
+                    Debug.Print("Deleted " + file.FullName);
                 }
                 catch (Exception ex)
                 {
-                    Debug.Log("Exception Thrown: " + ex.Message);
+                    Debug.Print("Exception Thrown: " + ex.Message);
 
                 }
 
             }
-            foreach (var dir in directoryInfo.GetDirectories())
+            foreach (var dir in DirectoryInfo.GetDirectories())
             {
                 try
                 {
                     dir.Delete(true);
-                    Debug.Log("Deleted Folder " + dir.FullName);
+                    Debug.Print("Deleted Folder " + dir.FullName);
                 }
                 catch (Exception ex)
                 {
-                    Debug.Log("Exception Thrown: " + ex.Message);
+                    Debug.Print("Exception Thrown: " + ex.Message);
                 }
 
             }
 
             return true;
-        
+
+        }
     }
+}
     
