f32efd127417ce9c41684ae142ffdb3312a2652e
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-18 11:15:09 -0400

    easier to detect if 64bit or 32bit is installed

--- Tasks/Scripts/BatFiles/removeedge.bat
+++ Tasks/Scripts/BatFiles/removeedge.bat
@@ -1,8 +1,10 @@
 ﻿@echo off
 
 :: Creator: ShadowWhisperer
-::  GitHub: https://github.com/ShadowWhisperer
-:: Updated: 12/09/2020
+:: GitHub: https://github.com/ShadowWhisperer
+:: Updated: 11/18/2021
+
+:: TODO: Check and show which versions were installed. 
 
 
 :: Check if ran as Admin
@@ -16,26 +18,30 @@ if %errorLevel% == 0 (
 
 :: Set Exist Variable - Check if Edge is intalled  *Show Message, if not installed
 set "EXIST=0"
-:: Stop Edge Task
+
+:: Stop Edge
 taskkill /im "msedge.exe" /f  >nul 2>&1
-::Do not install Edge from Windows Updates
+
+::Do not reinstall Edge from Windows Update
 reg add HKLM\SOFTWARE\Microsoft\EdgeUpdate /t REG_DWORD /v DoNotUpdateToEdgeWithChromium /d 1 /f >nul 2>&1
 
 :: Uninstall - 32Bit
 if exist "C:\Program Files (x86)\Microsoft\Edge\Application\" (
 set "EXIST=1"
+echo Microsoft Edge (32bit) has been found and will start removing.
 for /f "delims=" %%a in ('dir /b "C:\Program Files (x86)\Microsoft\Edge\Application\"') do (
 cd /d "C:\Program Files (x86)\Microsoft\Edge\Application\%%a\Installer\"
 if exist "setup.exe" (
 echo - Removing Microsoft Edge
 start /w setup.exe --uninstall --system-level --force-uninstall)
-echo Finished
+echo Finished.
 timeout /t 3 & exit
 ))
 
 :: Uninstall - 64Bit
 if exist "C:\Program Files\Microsoft\Edge\Application\" (
 set "EXIST=1"
+echo Microsoft Edge (64bit) has been found and will start removing.
 for /f "delims=" %%a in ('dir /b "C:\Program Files\Microsoft\Edge\Application\"') do (
 cd /d "C:\Program Files\Microsoft\Edge\Application\%%a\Installer\"
 if exist "setup.exe" (
@@ -48,5 +54,5 @@ timeout /t 3 & exit
 
 :: Not Installed
 if "%EXIST%"=="0" (
-echo. & echo Edge Is Not Installed & echo.
+echo. & echo Edge is not installed. & echo.
 timeout /t 3 & exit)
