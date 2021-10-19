0daaad5c133980babe63df9278a78a01c388baea
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-19 10:11:30 -0400

    Started SystemTools

--- Tasks/Tasksv3/Classes/SystemTools.cs
+++ Tasks/Tasksv3/Classes/SystemTools.cs
@@ -10,7 +10,29 @@ namespace Tasks.Cleanup_Modules
 {
     public class SystemTools
     {
+   // This class is for system tools that are based on Windows functions, such as taskkilling, browser removal, etc.
+  
+        // Proper Syntax: CreateProcess("explorer.exe") 
+        public static void CreateProcess(string processname)
+        {
+            
+        }
+        
+        // Proper Syntax: EndProcess("explorer.exe")
+        public static void EndProcess(string processname)
+        {
+            
+        }
+        
+        // Proper Syntax: EndBrowserProcess(1);
+       public static void EndBrowserProcess(int Browser)
+       {
+           // 1 - Chrome 
+           // 2 - Firefox
+           // 3 - Edge
     
+       }
+        
     }
  }
 
