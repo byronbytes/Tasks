b101cd5e21055379030e5b5cb2ca3586894d32d3
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-19 09:51:33 -0400

    base class

---
+++ Tasks/Tasksv3/Classes/SystemTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Cleanup_Modules
+{
+    public class SystemTools
+    {
+    
+    }
+ }
+
