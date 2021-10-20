ccbbdd92529649237d7461ca5c4eb43aa259d2d0
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-19 22:24:51 -0400

    Working on functions

--- Tasks/Tasksv3/Classes/SystemTools.cs
+++ Tasks/Tasksv3/Classes/SystemTools.cs
@@ -12,16 +12,19 @@ namespace Tasks.Cleanup_Modules
     {
    // This class is for system tools that are based on Windows functions, such as taskkilling, browser removal, etc.
   
+        //Variables:
+        public string task = "";
+        
         // Proper Syntax: CreateProcess("explorer.exe") 
         public static void CreateProcess(string processname)
         {
-            
+          processname = task;      
         }
         
         // Proper Syntax: EndProcess("explorer.exe")
         public static void EndProcess(string processname)
         {
-            
+            processname = task;
         }
         
         // Proper Syntax: EndBrowserProcess(1);
