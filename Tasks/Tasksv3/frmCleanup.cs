caf6a3431496a52aa3a671b2ac19ec53808d7229
an unknown dude <67998246+unknownbruh@users.noreply.github.com>
2021-10-20 11:24:10 +0700

    code cleanup and minor changes

--- Tasks/Tasksv3/frmCleanup.cs
+++ Tasks/Tasksv3/frmCleanup.cs
@@ -15,79 +15,58 @@ namespace Tasks.Tasks_v3._0._0 {
 
         private void frmNewCleanup_Load(object sender, EventArgs e) {}
   
-        // DeleteAllFiles is prone to change and be replaced with a new and more efficient method.
-        // 
-        private (int, int, int, int) DeleteAllFiles(DirectoryInfo directoryInfo)
-        {
+        // @experimental: Expect changes to the method
+        private (int, int, int, int) DeleteAllFiles(DirectoryInfo directoryInfo) {
             int deletedFile = 0;
             int deletedDir = 0;
             int fileCount = directoryInfo.GetFiles().Length;
             int dirCount = directoryInfo.GetDirectories().Length;
             
-            foreach (var file in directoryInfo.GetFiles())
-            {
-                try
-                {
+            foreach (var file in directoryInfo.GetFiles()) {
+                try {
                     file.Delete();
                     listBox1.Items.Add("Deleted file '" + file.FullName + "'");
                     ++deletedFile;
-                }
-                catch (Exception ex)
-                {
-                    listBox1.Items.Add("Exception Thrown: " + ex.Message);
-
-                }
-
+                } catch (Exception ex) { listBox1.Items.Add("Exception Thrown: " + ex.Message); }
             }
-            foreach (var dir in directoryInfo.GetDirectories())
-            {
-                try
-                {
+            
+            foreach (var dir in directoryInfo.GetDirectories()) {
+                try {
                     dir.Delete(true);
                     listBox1.Items.Add("Deleted folder '" + dir.FullName + "'");
                     ++deletedDir;
-                }
-                catch (Exception ex)
-                {
-                    listBox1.Items.Add("Exception Thrown: " + ex.Message);
-                }
+                } catch (Exception ex) { listBox1.Items.Add("Exception Thrown: " + ex.Message); }
             }
             
             return (deletedFile, fileCount, deletedDir, dirCount);
         }
         
-        private void button3_Click(object sender, EventArgs e)
-        {
+        private void button3_Click(object sender, EventArgs e) {
             // The deleting / checkbox method is also prone to change soon.
-            if (cbTempFiles.Checked)
-            {
+            if (cbTempFiles.Checked) {
                 var (wtdf, wtfc, wtdd, wtdc) = DeleteAllFiles(new DirectoryInfo("C:\\Windows\\Temp")); 
                 var (utdf, utfc, utdd, utdc) = DeleteAllFiles(new DirectoryInfo(Path.GetTempPath()));
 
                 listBox1.Items.Add("Temp Files Deleted.");
                 CleanupLog((wtdf+utdf), (wtfc+utfc), (wtdd+utdd), (wtdc+utdc));
             }
-
         }
 
 
         // This method will deprecate the need for listboxes, even invisible ones.
         // as a good coder once said "NEVER NEVER NEVER use a UI object for a non-UI method.
-        public void CleanupLog(int df, int fc, int dd, int dc)
-        {
+        public void CleanupLog(int df, int fc, int dd, int dc) {
             CleanupSummary();
             label9.Text = "Deleted " + df + "/" + fc + "files and " + dd + "/" + dc + " directories.";
         }
             
             
-        public void CleanupSummary()
-        {
-            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
-            int secondsSinceEpoch = (int)t.TotalSeconds;
-            string folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            string folderTasks = Path.Combine(folder, "Tasks");
-            string folderTasksCleanup = Path.Combine(folderTasks, "Cleanup Summary");
-            File.WriteAllLines(folderTasksCleanup + "\\cleanupSummary" + secondsSinceEpoch + ".txt", listBox1.Items.Cast<string>().ToArray());
+        public void CleanupSummary() {
+            int t = (int) ((DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds);
+            File.WriteAllLines(
+              Path.Combine(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Tasks"), "Cleanup Summary") + "\\tasks-cleanup-summary-" + t + ".txt", 
+              listBox1.Items.Cast<string>().ToArray()
+            );
         }
     }
 }
