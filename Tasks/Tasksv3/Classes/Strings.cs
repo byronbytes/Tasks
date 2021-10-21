1d70a8b4e3023e34188af9c1580b33ca8844d3cb
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-21 13:11:38 -0400

    update dev branch (#23)
    * Bug fix
    
    * Delete RemoveExtension.cs
    
    * Update frmAnalyze.cs
    
    * Create strings.cs
    
    * Rename strings.cs to Strings.cs
    
    * Create Strings.cs

---
+++ Tasks/Tasksv3/Classes/Strings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Cleanup_Modules
+{
+    public class Strings
+    {
+   // This class is for strings.
+  
+        public static void Strings()
+        {
+            // Analyze Found
+          public string 1 = "Your computer is currently running on battery power. It is suggested to put it on a charger."; 
+          public string 2 = "Your computer is currently running on a limited power mode.";
+          public string 3 = "Your computer does not have file compression turned on.";
+          public string 4 = "Your computer currently has" + numdrives + "drive(s) currently plugged in.";
+          public string 5 = "Your temp folders have " + addfolders +  ""
+              
+              
+              
+              
+              
+              
+        }
+        
+        
+    }
+ }
+
