a5442577271c8769d35d51831022b3a161971a27
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-21 08:24:48 -0400

    Create strings.cs

---
+++ Tasks/Tasksv3/Classes/strings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Cleanup_Modules
+{
+    public class Strings
+    {
+   // This class is for strings.
+  
+        public static void Strings()
+        {
+          public string 1 = "Your computer is currently running on battery power. It is suggested to put it on a charger."; 
+          public string 2 = "Your computer is currently running on a limited power mode.";
+          public string 3 = "Your computer does not have file compression turned on."
+        }
+        
+        
+    }
+ }
+
