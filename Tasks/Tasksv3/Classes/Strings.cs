bc84b4943c6e24e2d68d254a80a6b65a89a45074
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-21 10:51:24 -0400

    Create Strings.cs

--- Tasks/Tasksv3/Classes/Strings.cs
+++ Tasks/Tasksv3/Classes/Strings.cs
@@ -14,9 +14,18 @@ namespace Tasks.Cleanup_Modules
   
         public static void Strings()
         {
+            // Analyze Found
           public string 1 = "Your computer is currently running on battery power. It is suggested to put it on a charger."; 
           public string 2 = "Your computer is currently running on a limited power mode.";
-          public string 3 = "Your computer does not have file compression turned on."
+          public string 3 = "Your computer does not have file compression turned on.";
+          public string 4 = "Your computer currently has" + numdrives + "drive(s) currently plugged in.";
+          public string 5 = "Your temp folders have " + addfolders +  ""
+              
+              
+              
+              
+              
+              
         }
         
         
