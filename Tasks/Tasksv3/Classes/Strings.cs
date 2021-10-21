e40957192808328e940674546d141fb8e8ee9019
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-21 14:14:55 -0400

    Overhaul + Strings

--- Tasks/Tasksv3/Classes/Strings.cs
+++ Tasks/Tasksv3/Classes/Strings.cs
@@ -10,23 +10,53 @@ namespace Tasks.Cleanup_Modules
 {
     public class Strings
     {
-   // This class is for strings.
+   // This class is for strings, any types of strings will be listed here.
   
-        public static void Strings()
+        public void AnalyzeStrings()
         {
-            // Analyze Found
+          // Analyze Issues Found
           public string 1 = "Your computer is currently running on battery power. It is suggested to put it on a charger."; 
           public string 2 = "Your computer is currently running on a limited power mode.";
           public string 3 = "Your computer does not have file compression turned on.";
           public string 4 = "Your computer currently has" + numdrives + "drive(s) currently plugged in.";
-          public string 5 = "Your temp folders have " + addfolders +  ""
-              
-              
-              
-              
-              
-              
+          public string 5 = "Your temp folders are taking up " + addfolders +  " of space.";
+          public string 6 = "Placeholder Text.";
+        
+            
+          // Analyze No Issues
+          public string perfect = "Your computer has no performance issues!";
+          public string finish = "Your computer has finished analyzing.";
+          public string perfect2 = "Tasks has optimized your computer.";
+            
+            
+           
         }
+        
+        
+        public void AppTypes() 
+        {
+            public string Chrome = "Chrome";
+            public string ChromeCanary = "Chrome Canary";
+            
+             
+            public string SpotifyApp = "Spotify";
+            public string SpotifyWindows = "Spotify (Windows Store Edition)";
+            
+        }
+        
+        public void AppExists()
+        {
+         //if(dir exists Chrome thingy)
+            // {
+            
+            // }
+            
+            
+        }
+        
+        
+           
+         
         
         
     }
