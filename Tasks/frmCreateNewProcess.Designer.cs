5ca7edde2b01842b324efbb23112ac0f4a3501fc
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-24 19:03:05 -0400

    Semi Functional "Create New Task"

--- Tasks/frmCreateNewProcess.Designer.cs
+++ Tasks/frmCreateNewProcess.Designer.cs
@@ -57,6 +57,7 @@ namespace Tasks
             this.button1.TabIndex = 1;
             this.button1.Text = "Create";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- Tasks/frmCreateNewProcess.cs
+++ Tasks/frmCreateNewProcess.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,11 @@ namespace Tasks
         private void button2_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start(textBox1.Text);
         }
     }
 }
