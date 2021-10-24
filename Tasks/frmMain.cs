473a1c6efb3d8314570b401b5dc77d1b9fa0138b
an unknown dude <67998246+unknownbruh@users.noreply.github.com>
2021-10-24 08:12:33 +0700

    Actual code cleanup in a while

--- Tasks/frmMain.cs
+++ Tasks/frmMain.cs
@@ -12,54 +12,19 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 // TODO: Cleanup and change the code style
-namespace Tasks
-{
-    public partial class frmMain : Form
-    {
+namespace Tasks {
+    public partial class frmMain : Form {
       
-        public frmMain()
-        {
-            InitializeComponent();
+        public frmMain() { InitializeComponent(); }
 
-        }
+        private void frmMain_Load(object sender, EventArgs e) {}
+        
+        private void button1_Click(object sender, EventArgs e) { new frmCleanup().Show(); }
+        private void button3_Click(object sender, EventArgs e) { new frmStartupPrograms().Show(); }
+        private void button4_Click(object sender, EventArgs e) { new frmTaskManager().Show(); }
+        private void button6_Click(object sender, EventArgs e) { new frmAbout().Show(); }
 
-        private void frmMain_Load(object sender, EventArgs e)
-        {
-        }
-        private void button1_Click(object sender, EventArgs e)
-        {
-            frmCleanup Cleanup = new frmCleanup(); // Initialize cleanup form.
-            Cleanup.Show(); // Show Form
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            frmTaskManager TaskManager = new frmTaskManager(); // Initialize cleanup form.
-            TaskManager.Show(); // Show Form
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            frmStartupPrograms Startups = new frmStartupPrograms();
-            Startups.Show();
-
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            frmAbout About = new frmAbout();
-            About.Show();
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-            Tasks_v3._0._0.frmInterface NewMenu = new Tasks_v3._0._0.frmInterface();
-           NewMenu.Show();
-        }
+        private void label1_Click(object sender, EventArgs e) { new Tasks_v3._0._0.frmInterface().Show(); }
+        private void label5_Click(object sender, EventArgs e) {}
     }
 }
