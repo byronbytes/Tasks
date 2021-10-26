5b7f67efca7275a020b7f15ddba3fc27d7046834
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-26 08:48:29 -0400

    Update TasksPatcher.ps1

--- Tasks/Scripts/Debloater/TasksPatcher.ps1
+++ Tasks/Scripts/Debloater/TasksPatcher.ps1
@@ -1,1 +1,16 @@
 Write-Host "This is the proprietary Tasks patching tool, this tool will attempt to fix Remove Bloat issues and bugs."
+
+$CurrentPolicy = Get-ExecutionPolicy
+$BlockedPolicy = "Restricted"
+
+<#This is currently a WIP, I have little to no resources, and will be changed throughout time.#>
+
+  if ($CurrentPolicy = Restricted)
+  {
+  Set-ExecutionPolicy -ExecutionPolicy Bypass
+  }
+  
+  if ($CurrentPolicy = Bypass)
+  {
+  Write-Host "Policy is set to bypass, no need for patching."
+  }
