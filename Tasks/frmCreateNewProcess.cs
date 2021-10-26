2d736c05e340df65b0e61632411846d51a6c7943
an unknown dude <67998246+unknownbruh@users.noreply.github.com>
2021-10-26 20:20:57 +0700

    code cleanup

--- Tasks/frmCreateNewProcess.cs
+++ Tasks/frmCreateNewProcess.cs
@@ -9,51 +9,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Tasks
-{
-    public partial class frmCreateNewProcess : Form
-    {
-        public frmCreateNewProcess()
-        {
-            InitializeComponent();
-        }
+namespace Tasks {
+    public partial class frmCreateNewProcess : Form {
+        public frmCreateNewProcess() { InitializeComponent(); }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Executables|*.exe*" })
-            {
+        private void button2_Click(object sender, EventArgs e) {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Executables|*.exe*" }) {
                 // If statement because if you closed it would throw an exception
-                if (ofd.ShowDialog() == DialogResult.OK)
-                {
-                    try
-                    {
-                        string task = ofd.FileName.ToString();
-                        Process.Start(task);
-                    }
-                    catch(Exception ex)
-                    {
-                        MessageBox.Show("An error has occurred: " + ex.Message);
-                    }
-                   
-                }
-                else
-                {
-                    // Do Nothing.
-                }
+                if(ofd.ShowDialog() == DialogResult.OK)
+                    try { Process.Start(ofd.FileName.ToString()); } catch(Exception ex) { MessageBox.Show("An error has occurred: " + ex.Message); }
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-        
-         try
-           {
-             Process.Start(textBox1.Text);
-           } 
-            catch(Exception ex)
-            {
-                 MessageBox.Show("An error has occurred: " + ex.Message);
-            }
+        private void button1_Click(object sender, EventArgs e) {
+         try { Process.Start(textBox1.Text); } catch(Exception ex) { MessageBox.Show("An error has occurred: " + ex.Message); }
         }
     }
 }
