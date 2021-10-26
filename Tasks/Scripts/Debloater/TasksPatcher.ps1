77afb5db1299b606e24e969c0cba7f87a1cedf8e
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-26 09:08:53 -0400

    Made it a try-catch statement to prevent errors

--- Tasks/Scripts/Debloater/TasksPatcher.ps1
+++ Tasks/Scripts/Debloater/TasksPatcher.ps1
@@ -6,18 +6,40 @@ Repository: https://github.com/LiteTools/Tasks
 
 Write-Host "This is the proprietary Tasks powershell patching tool, this tool will attempt to fix Remove Bloat issues and bugs with powershell."
 
+  if (-not(Check-IsElevated))
+ { 
+  Write-Host "WARN: It is highly suggested you run this script as administrator." 
+ }
+ 
 $CurrentPolicy = Get-ExecutionPolicy
 $BlockedPolicy = "Restricted"
+
 
 <#This is currently a WIP, I have little to no resources, and will be changed throughout time.#>
 
   if ($CurrentPolicy = Restricted)
  {
-    Set-ExecutionPolicy -ExecutionPolicy Bypass
-    Write-Host "Execution Policy has now changed to bypass."
+   try 
+      {
+        Set-ExecutionPolicy -ExecutionPolicy Bypass
+        Write-Host "Execution Policy has now changed to bypass."
+      }
+      catch
+        {
+          Write-Host "An error has occurred and Tasks was unable to patch."
+          
+        }
  }
   
   if ($CurrentPolicy = Bypass)
  {
-    Write-Host "Execution Policy is already set to Bypass, no need for patching."
- }
+    try
+      {
+        Write-Host "Execution Policy is already set to Bypass, no need for patching."
+      }
+      catch
+        {
+          Write-Host "An error occurred."
+         
+        }
+  }
