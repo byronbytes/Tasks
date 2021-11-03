cb595c0bdf00224c8a4a779fce92fc275f946338
Byron <53088136+byronbytes@users.noreply.github.com>
2021-11-03 15:50:34 -0400

    [IMPORTANT] Removed v3.0 references and progress
    I had decided to remove most of the v3 stuff. This is because I feel like that it's turning into a huge multitask and I don't know what to do sometimes, so to clear up confusion I removed all v3 stuff, I have all the code backed up somewheres safe so all progress isn't gone.

--- Tasks/frmMain.cs
+++ Tasks/frmMain.cs
@@ -25,7 +25,6 @@ namespace Tasks {
 
         private void label1_Click(object sender, EventArgs e) 
         { 
-            new Tasks_v3._0._0.frmInterface().Show();
         }
         private void label5_Click(object sender, EventArgs e) {}
     }
