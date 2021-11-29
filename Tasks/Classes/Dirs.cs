d5b38ccde835ef177bb845699a36dbf39ed42d15
Byron <53088136+byronbytes@users.noreply.github.com>
2021-11-29 13:26:51 -0500

    Added quick addition

--- Tasks/Classes/Dirs.cs
+++ Tasks/Classes/Dirs.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace Tasks
 {
     class Dirs
@@ -14,9 +13,11 @@ namespace Tasks
         public static string chromeDir;
         public static string edgeDir;
         public static string discordDir;
-        // Directories that point to the extension folder
+        // Directories that point to the extensions folder:
         public static string firefoxExtDir;
         public static string chromeExtDir;
-        public static string edgeExtDir;
+        public static string edgeExtDir;  
+        // Debugging Purposes:
+        public static string tasksDir;
     }
 }
