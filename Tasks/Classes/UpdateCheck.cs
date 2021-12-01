d80a4dd9c7dacffacd2b5627cc633c2fa6d31c10
Byron <53088136+byronbytes@users.noreply.github.com>
2021-12-01 13:22:07 -0500

    Create UpdateCheck.cs

---
+++ Tasks/Classes/UpdateCheck.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class UpdateCheck
+    {
+       public static void CheckForUpdates()
+       {
+       // This will check for updates via a dropbox server. I'm going to update this method later in development, since dropbox is most likely unstable and 
+       has security flaws.
+       }
+       
+       public static void ValidifyUpdate()
+       {
+      // This will validify that the update is not from a development build.
+        }
+       
+    }
+}
