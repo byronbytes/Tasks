054d24cd6531dce8757b86b4b54cc95a89942c84
Byron <53088136+byronbytes@users.noreply.github.com>
2021-12-01 10:25:39 -0500

    Adding TasksDirectories
    will remove that class soon, since it can be merged here.

--- Tasks/Classes/Dirs.cs
+++ Tasks/Classes/Dirs.cs
@@ -17,7 +17,8 @@ namespace Tasks
         public static string firefoxExtDir;
         public static string chromeExtDir;
         public static string edgeExtDir;  
-        // Debugging Purposes:
-        public static string tasksDir;
+        // Tasks Directories:
+        public static string tasksDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Tasks";
+        public static string tasksCleanup = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Tasks\\Cleanup Summary";
     }
 }
