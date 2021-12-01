1870238c3d636f65bc2b80763c16cde3800965bf
Byron <53088136+byronbytes@users.noreply.github.com>
2021-12-01 14:15:46 -0500

    Update UpdateCheck.cs

--- Tasks/Classes/UpdateCheck.cs
+++ Tasks/Classes/UpdateCheck.cs
@@ -11,12 +11,17 @@ namespace Tasks
        public static void CheckForUpdates()
        {
        // This will check for updates via a dropbox server. I'm going to update this method later in development, since dropbox is most likely unstable and 
-       has security flaws.
+           // does not install properly.
        }
        
        public static void ValidifyUpdate()
        {
-      // This will validify that the update is not from a development build.
+      // This will validify that the update is not from a development build. This is a current draft.
+           if(lblNewVersion.Contains("dev") == true)
+           {
+              btnDownload.Visible = false;
+           }
+           
         }
        
     }
