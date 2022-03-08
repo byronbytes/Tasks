d220c26289a03c7e592623c408a3a5f059868c87
Matty <53088136+byronbytes@users.noreply.github.com>
2022-03-08 13:24:59 -0500

    Create StartupPrograms.cs

---
+++ Tasks/Core/StartupPrograms.cs
@@ -0,0 +1,21 @@
+﻿/*
+    (c) LiteTools 2022 (https://github.com/LiteTools)
+    All rights reserved under the GNU General Public License v3.0.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tasks.Core
+{
+    class StartupPrograms
+    {
+    
+    }
+}
