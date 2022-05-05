7bd5e39f7afd6f626b5504bd494b5afe4c3657a4
Matty <53088136+byronbytes@users.noreply.github.com>
2022-05-05 09:10:07 -0400

    Update Dirs.cs

--- Tasks/Dirs.cs
+++ Tasks/Dirs.cs
@@ -1,5 +1,7 @@
-// (c) LiteTools 2021
-// All rights reserved under the Apache-2.0 license.﻿
+/*
+    (c) LiteTools 2022 (https://github.com/LiteTools)
+    All rights reserved under the GNU General Public License v3.0.
+*/
 
 using System;
 using System.Collections.Generic;
@@ -28,6 +30,7 @@ namespace Tasks
         public static string tasksCleanup = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Roaming\\Tasks\\Cleanup Summary\\";
     }
 
+    
     // Going to be merging most of the stuff into a new directory method.
     class Directories
     {
@@ -40,6 +43,16 @@ namespace Tasks
         public static string ProgramFiles86 = "C:\\Program Files (x86)\\";
         public static string StartupFolder = ""; //Environment exists will add soon
         public static string tasksDir = "C:\\Program Files (x86)\\Tasks\\";
+        // Directories for applications
+        public static string ChromeDir = "";
+        public static string FirefoxDir = "";
+        public static string EdgeDir = "";
+        public static string DiscordDir = "";
+        public static string VLCDir = "";
+        public static string SpotifyDir1 = "";
+        public static string ChromeExtDir = "";
+        public static string FirefoxExtDir = "";
+        public static string EdgeExtDir = "";
   
     }
 }
