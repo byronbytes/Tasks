2a637fe43e8db0b78d675bf26c5fdde25efc56c9
Matty <53088136+byronbytes@users.noreply.github.com>
2022-05-18 12:35:42 -0400

    Update UpdateUtils.cs

--- Tasks/Core/Utils/UpdateUtils.cs
+++ Tasks/Core/Utils/UpdateUtils.cs
@@ -11,15 +11,26 @@ namespace Tasks.Core.Utils
 
         public static string CurrentVer = "4.1.0";
 
-        public static void CheckForUpdates()
+        public static bool CheckForUpdates()
         {
-
+            // Calls the update check
+            
+            // Here's a basic check for updating, if it even gets added soon.
+            if(CurrentVer == "4.2.0")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public static void Update()
         {
             bool Updated;
             // Calls the update function
+            
         }
 
 
