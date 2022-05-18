fcbce625fdf9fd15472bf68a40f5c9ae3b2bc608
Matty <53088136+byronbytes@users.noreply.github.com>
2022-05-18 13:08:37 -0400

    Update CleanupUtils.cs

--- Tasks/Core/Utils/CleanupUtils.cs
+++ Tasks/Core/Utils/CleanupUtils.cs
@@ -9,14 +9,13 @@ namespace Tasks.Core.Utils
     public class CleanupUtils
     {
         // Deletes all files in a directory.
+        // Will also add a string list for all of the file names, that's pretty easy.
          public static bool DeleteAllFiles(DirectoryInfo directoryInfo)
         {
-
-            foreach (var file in directoryInfo.GetFiles())
+            foreach (var file in directoryInfo.GetFiles()) // could optimize?
             {
                 try
                 {
-                // Deletes all files in directory.
                     file.Delete();
                  //   Debug.Log(LogSuccess + file.FullName);
                 }
@@ -30,21 +29,37 @@ namespace Tasks.Core.Utils
             {
                 try
                 {
-                // Deletes all directories in a directory.
                     dir.Delete(true);
-                //    Debug.Log(LogSuccess + dir.FullName);
+                    // Debug.Log(LogSuccess + dir.FullName);
                 }
                 catch (Exception ex)
                 {
-               //   Debug.Log(LogError + ex.Message);
+                    //   Debug.Log(LogError + ex.Message);
                 }
 
             }
 
-            return true; // why does it return true, could this be expanded on?
+            return true; // why does it return true, could this be expanded on? 
         }
-
-
-
     }
+    // Really only used for modifying certain things that may need a restart.
+    // Might concider making a library for classes like this so it's easier to use these.
+    public static void RestartExplorer()
+    {
+        Process.Start("taskkill", "/f /im explorer.exe"); // Since Tasks runs as admin it's auto elevated.
+        Process.Start("explorer.exe");
+    }
+    
+    public static bool CanLogCleanup() // probably won't work lol
+    {
+        if(Directory.Exists(Dirs.TasksDir)) //half pseudo-code
+        {
+            return true; // Can be used for Settings
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
 }
