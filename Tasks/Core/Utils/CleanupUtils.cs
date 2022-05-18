b1ad46278e231fd51b5e4d31c5a722d125a56718
Matty <53088136+byronbytes@users.noreply.github.com>
2022-05-18 09:42:02 -0400

    a

--- Tasks/Core/Utils/CleanupUtils.cs
+++ Tasks/Core/Utils/CleanupUtils.cs
@@ -9,16 +9,47 @@ namespace Tasks.Core.Utils
     public class CleanupUtils
     {
         // Deletes all files in a directory.
-        public static void DeleteAllFiles()
+         private bool DeleteAllFiles(DirectoryInfo directoryInfo)
         {
 
-        }
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                try
+                {
+                // Deletes all files in directory.
+                    file.Delete();
+                    CleanupLogsLBox.Items.Add(LogSuccess + file.FullName);
+                }
+                catch (Exception ex)
+                {
+                    CleanupLogsLBox.Items.Add(LogError + ex.Message);
+                }
 
+            }
+            foreach (var dir in directoryInfo.GetDirectories())
+            {
+                try
+                {
+                // Deletes all directories in a directory.
+                    dir.Delete(true);
+                    Debug.Log(LogSuccess + dir.FullName);
+                }
+                catch (Exception ex)
+                {
+                    CleanupLogsLBox.Items.Add(LogError + ex.Message);
+                }
+
+            }
+
+            return true;
+        }
 
         public static void WriteOutput()
         {
 
         }
+
+        public static 
 
 
     }
