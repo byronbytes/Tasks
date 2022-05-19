6b3182b2f663d204ea262fc0d8100fba83cb71aa
Matty <53088136+byronbytes@users.noreply.github.com>
2022-05-19 13:17:15 -0400

    Create OtherUtils.cs

---
+++ Tasks/Core/Utils/OtherUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Core.Utils
+{
+    public class OtherUtils
+    {
+     
+    
+
+
+    }
+}
